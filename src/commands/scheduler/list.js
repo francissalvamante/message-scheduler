f48560f4dab7016ae0c4c52f39d503fc776e4f76
Francis Vianney Salvamante <salvamante2012@gmail.com>
2025-03-10 20:38:24 +0800

    Added list messages functionality and added a comment with an embed template to update the message sent

--- src/commands/scheduler/list.js
+++ src/commands/scheduler/list.js
@@ -1,10 +1,34 @@
+const Message = require("../../models/messages");
+const { EmbedBuilder } = require("discord.js");
+
 module.exports = {
   name: "list",
   description: "Show list of scheduled message.",
   callback: async (client, interaction) => {
-    interaction.reply("List");
+    console.log("interaction", interaction.member.guild.id);
+    const messages = await Message.find({
+      guildId: interaction.member.guild.id,
+    });
+
+    const embed = new EmbedBuilder().setTitle(
+      "List of all announcements/messages scheduled:"
+    );
+
+    messages.forEach((message, idx) => {
+      embed.addFields({
+        name: `${idx + 1}. ${message.title} - ${message._id}`,
+        value: "",
+        inline: false,
+      });
+    });
+
+    embed.setFooter({
+      text: "To update any message, use /update and provide the id located after the title",
+    });
+
+    interaction.reply({ embeds: [embed] });
   },
-  deleted: true,
+  // deleted: true,
   // devOnly: Boolean;
   // testOnly: Boolean,
   // options: Object[]
