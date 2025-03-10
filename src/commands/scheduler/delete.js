52286947b7f3cd5d37bd3708f3bc114a0cd6a196
Francis Vianney Salvamante <salvamante2012@gmail.com>
2025-03-10 21:39:25 +0800

    Added delete functionality

--- src/commands/scheduler/delete.js
+++ src/commands/scheduler/delete.js
@@ -1,10 +1,32 @@
+const { ApplicationCommandOptionType } = require("discord.js");
+const Message = require("../../models/messages");
+const logger = require("pino")();
+
 module.exports = {
   name: "delete",
   description: "Delete a scheduled message.",
+  options: [
+    {
+      name: "id",
+      description:
+        "ID of message to be deleted! (Be careful as this is irreversible)",
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    },
+  ],
   callback: async (client, interaction) => {
-    interaction.reply("Delete");
+    const id = await interaction.options.getString("id");
+
+    try {
+      await Message.findByIdAndDelete(id);
+      logger.info("🗑️ Successfully deleted message");
+    } catch (error) {
+      logger.error(`There was an error deleting the message: ${error}`);
+    }
+
+    interaction.reply("Message deleted!", { ephemeral: true });
   },
-  deleted: true,
+  // deleted: true,
   // devOnly: Boolean;
   // testOnly: Boolean,
   // options: Object[]
